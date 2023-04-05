80b6345d5231c15d750362365a1cc370dc3b14d5
정다훈 <ununbium@naver.com>
2023-04-05 11:50:02 +0900

    Add css rule
    css 내용및 여러가지 수정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 // pages는 한이음 프로젝트 화면, page는 캡스톤디자인 졸작 화면으로 사용을 필요하지 않는 것은 주석처리 해주세요.
-// import Home from './pages/Home';
-import Home from './page/Home';
+import Home from './pages/Home';
+// import Home from './page/Home';
 
 function App() {
   return (
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,60 +3,55 @@ import "../css/Menu.css";
 
 export const Menu = ({ ...props }) => {
   return (
+    
     <div className="menu">
-      <div className="category-top">
-        <div className="-----">단지 정보</div>
-
-        <div className="--------2">소음 확인·관리</div>
-
-        <div className="--------">소음 신고 내역</div>
-      </div>
-
-      <div className="category-bottom">
-        <div className="-----------">소음 발생 예정 내역</div>
-
-        <div className="------">입주민 관리</div>
-
-        <div className="--------">기타 추가 메뉴</div>
-      </div>
 
       <div className="login">
-        <div className="name-rectangle"></div>
-
-        <div className="date-rectangle"></div>
-
-        <div className="---">홍길동</div>
-
-        <div className="_2023--01--15-">2023년 01월 15일</div>
-
-        <div className="---button">
-          <div className="notice-rectangle"></div>
-
-          <div className="--">알림</div>
+        <div className="name-rectangle">
+          <div className="user">홍길동</div>
+        </div>
+        <div className="date-rectangle">
+          <div className="YDM">2023년 01월 15일</div>
         </div>
 
-        <div className="-----2">
-          <div className="------2">안읽은 알림</div>
-
-          <div className="_2">2</div>
-
+        <div className="sysAlarm1">
+          <div className="notice-rectangle">
+            <div className="alarms">알림</div>
+          </div>
+          <div className="URAlarm">안읽은 알림</div>
+          <div className="reportNums1">2</div>
           <div className="fix">건</div>
         </div>
 
-        <div className="---button">
-          <div className="notice-rectangle2"></div>
-
-          <div className="--2">신고</div>
-        </div>
-
-        <div className="-----2">
-          <div className="------3">안읽은 신고</div>
-
-          <div className="_1">1</div>
-
+        <div className="sysAlarm2">
+          <div className="notice-rectangle2">
+            <div className="report">신고</div>
+          </div>
+          <div className="URReport">안읽은 신고</div>
+          <div className="reportNums2">1</div>
           <div className="fix2">건</div>
         </div>
+        </div>
+
+        <div className="AlarmsPage">
       </div>
+
+      <div className="category-top">
+        <div className="JInfo">단지 정보</div>
+
+        <div className="IAMN">소음 확인·관리</div>
+
+        <div className="NCH">소음 신고 내역</div>
+      </div>
+
+      <div className="category-bottom">
+        <div className="sngList">소음 발생 예정 내역</div>
+
+        <div className="MRds">입주민 관리</div>
+
+        <div className="NCH">기타 추가 메뉴</div>
+      </div>
+
       <svg
         className="line-1"
         width="670"
