a42762e8be66de53e8a76210badaa0ad870f6a75
정다훈 <ununbium@naver.com>
2023-04-05 12:59:52 +0900

    Add css system
    망할 것

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -18,49 +18,38 @@ export const Menu = ({ ...props }) => {
           <div className="notice-rectangle">
             <div className="alarms">알림</div>
           </div>
-          <div className="URAlarm">안읽은 알림</div>
-          <div className="reportNums1">2</div>
-          <div className="fix">건</div>
+          <div className="URalarms">
+            안읽은 알람 <div className="numscolor">2</div> 건
+          </div>
         </div>
 
         <div className="sysAlarm2">
           <div className="notice-rectangle2">
             <div className="report">신고</div>
           </div>
-          <div className="URReport">안읽은 신고</div>
-          <div className="reportNums2">1</div>
-          <div className="fix2">건</div>
+          <div className="URreport">
+            안읽은 신고 <div className="numscolor">2</div> 건
+          </div>
         </div>
+      </div>
+
+      <div className="SUList">
+        <div className="category-top">
+          <div className="JInfo">단지 정보</div>
+
+          <div className="IAMN">소음 확인·관리</div>
+
+          <div className="NCH">소음 신고 내역</div>
         </div>
 
-        <div className="AlarmsPage">
+        <div className="category-bottom">
+          <div className="sngList">소음 발생 예정 내역</div>
+
+          <div className="MRds">입주민 관리</div>
+
+          <div className="NCH">기타 추가 메뉴</div>
+        </div>
       </div>
-
-      <div className="category-top">
-        <div className="JInfo">단지 정보</div>
-
-        <div className="IAMN">소음 확인·관리</div>
-
-        <div className="NCH">소음 신고 내역</div>
-      </div>
-
-      <div className="category-bottom">
-        <div className="sngList">소음 발생 예정 내역</div>
-
-        <div className="MRds">입주민 관리</div>
-
-        <div className="NCH">기타 추가 메뉴</div>
-      </div>
-
-      <svg
-        className="line-1"
-        width="670"
-        viewBox="0 0 670 0"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 0H670" stroke="#E7E7E7" />
-      </svg>
     </div>
   );
 };
